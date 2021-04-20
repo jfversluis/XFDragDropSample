e24252767bda25756153d99a75639db55117a253
Gerald Versluis <gerald@verslu.is>
2021-04-20 16:51:41 +0200

    Implement drag & drop

--- XFDragDropSample/MainPage.xaml.cs
+++ XFDragDropSample/MainPage.xaml.cs
@@ -14,5 +14,21 @@ namespace XFDragDropSample
         {
             InitializeComponent();
         }
+
+        void DragGestureRecognizer_DragStarting(System.Object sender, Xamarin.Forms.DragStartingEventArgs e)
+        {
+            var label = (sender as Element)?.Parent as Label;
+            e.Data.Properties.Add("Text", label.Text);
+        }
+
+        void DropGestureRecognizer_Drop(System.Object sender, Xamarin.Forms.DropEventArgs e)
+        {
+            var data = e.Data.Properties["Text"].ToString();
+            var frame = (sender as Element)?.Parent as Frame;
+            frame.Content = new Label
+            {
+                Text = data
+            };
+        }
     }
 }
